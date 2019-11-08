6ffc27c9485924d115986123383541529b50d15f
dadnotorc <dadnotorc@gmail.com>
2019-11-08 00:03:39 -0500

    lintcode/_0575_DecodeString 加入迭代解法

--- src/lintcode/_0575_DecodeString.java
+++ src/lintcode/_0575_DecodeString.java
@@ -7,6 +7,8 @@ package lintcode;
 
 import org.junit.Test;
 
+import java.util.Stack;
+
 /**
  * 575. Decode String
  *
@@ -30,31 +32,70 @@ import org.junit.Test;
  */
 public class _0575_DecodeString {
 
-    // todo
-    public String expressionExpand(String s) {
+    // 解法一 迭代
+    public String expressionExpand_iterative(String s) {
         if (s == null || s.length() == 0)
             return "";
 
-        StringBuilder sb = new StringBuilder();
+        Stack<Object> stack = new Stack<>();
+        int repNum = 0;
 
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                repNum = repNum * 10 + c - '0';
+            } else if (c == '[') {
+                stack.push(repNum);
+                repNum = 0;
+            } else if (c == ']') {
+                String tmpStr = popStr(stack);
+                int num = (Integer) stack.pop();
+
+                for (int i = 0; i < num; i++) {
+                    stack.push(tmpStr);
+                }
+            } else {
+                stack.push(String.valueOf(c));
+            }
+        }
+
+        return popStr(stack);
+    }
+
+    // 获得stack上层里非数字的字符串
+    private String popStr(Stack<Object> stack) {
+        Stack<String> buffer = new Stack<>();
+        while (!stack.isEmpty() && stack.peek() instanceof String) {
+            buffer.push((String) stack.pop()); // 第一次转向
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!buffer.isEmpty()) {
+            sb.append(buffer.pop()); // 第二次转向
+        }
+        // 二次转向后, 字符串顺序与原顺势一致
         return sb.toString();
+    }
+
+    // todo 解法二 递归
+    public String expressionExpand_recursion(String s) {
+
     }
 
     @Test
     public void test1() {
         String exp = "abcaaa";
-        org.junit.Assert.assertTrue(exp.equals(expressionExpand("abc3[a]")));
+        org.junit.Assert.assertTrue(exp.equals(expressionExpand_iterative("abc3[a]")));
     }
 
     @Test
     public void test2() {
         String exp = "adadpfpfpfadadpfpfpfadadpfpfpfxyz";
-        org.junit.Assert.assertTrue(exp.equals(expressionExpand("3[2[ad]3[pf]]xyz")));
+        org.junit.Assert.assertTrue(exp.equals(expressionExpand_iterative("3[2[ad]3[pf]]xyz")));
     }
 
     @Test
     public void test3() {
         String exp = "xaaaaaaaaaaaay";
-        org.junit.Assert.assertTrue(exp.equals(expressionExpand("x12[a]y")));
+        org.junit.Assert.assertTrue(exp.equals(expressionExpand_iterative("x12[a]y")));
     }
 }
