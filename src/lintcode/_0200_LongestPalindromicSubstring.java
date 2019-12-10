0b1bcb41cd8b6236f91abb3cfce89fe1bc9c947e
dadnotorc <dadnotorc@gmail.com>
2019-12-09 23:01:54 -0500

    修改答案: src/lintcode/_0200_LongestPalindromicSubstring.java

--- src/lintcode/_0200_LongestPalindromicSubstring.java
+++ src/lintcode/_0200_LongestPalindromicSubstring.java
@@ -23,113 +23,165 @@ import org.junit.Test;
  * Output:"aba"
  *
  * Challenge
- * - O(n2) time is acceptable. Can you do it in O(n) time.
+ * - O(n^2) time is acceptable. Can you do it in O(n) time.
  */
 public class _0200_LongestPalindromicSubstring {
 
-    // 中心枚举法
+    // todo 读 https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
+
+    /**
+     * 中心枚举法
+     * time:  O(n^2)
+     * space: O(1)
+     */
     public String longestPalindrome(String s) {
         if (s == null || s.length() <= 1)
             return s;
 
-        String ans = "";
+        int ansStartIndex = 0, curLen = 0, maxLen = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            String sub = findPalindrome(s, i);
-            if (sub.length() > ans.length()) {
-                ans = sub;
-            }
+             curLen = findPalindrome(s, i, i);
+             if (curLen > maxLen) {
+                 maxLen = curLen;
+                 ansStartIndex = i - maxLen / 2;
+             }
+
+             curLen = findPalindrome(s, i, i + 1);
+             if (curLen > maxLen) {
+                 maxLen = curLen;
+                 ansStartIndex = i - maxLen / 2 + 1;
+             }
         }
 
-        return ans;
+        return s.substring(ansStartIndex, ansStartIndex + maxLen);
     }
 
-    // todo  有 bug
-    public String findPalindrome(String s, int index) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(s.charAt(index));
-        int l = index - 1, r = index + 1;
-        if (r < s.length() && s.charAt(index) == s.charAt(r)) // 注意 要判断 r 是否越界
-            sb.append(s.charAt(r++));
+    // 给出当前index, 找出可向左右延伸最长距离
+    public int findPalindrome(String s, int l, int r) {
+        int len = 0;
 
         while (l >= 0 && r < s.length()) {
-            if (s.charAt(l) != s.charAt(r))
+            if (s.charAt(l) != s.charAt(r)) {
                 break;
+            }
 
-            sb.insert(0, s.charAt(l--));
-            sb.append(s.charAt(r++));
+            len += l == r ? 1 : 2; // 注意判断当前左右指针是否重合
+            l--;
+            r++;
         }
-        return sb.toString();
+
+        return len;
     }
 
     @Test
     public void test0() {
         String s = "a";
-        String exp = "a";
-        org.junit.Assert.assertEquals(exp, longestPalindrome(s));
+        org.junit.Assert.assertEquals("a", longestPalindrome(s));
     }
 
     @Test
     public void test1() {
-        String s = "aa";
-        String exp = "aa";
-        org.junit.Assert.assertEquals(exp, longestPalindrome(s));
+        String s = "banana";
+        System.out.println("act=" + longestPalindrome(s));
+        org.junit.Assert.assertTrue("anana".equals(longestPalindrome(s)));
     }
 
     @Test
     public void test2() {
+        String s = "million";
+        System.out.println("act=" + longestPalindrome(s));
+        org.junit.Assert.assertTrue("illi".equals(longestPalindrome(s)));
+    }
+
+    @Test
+    public void test3() {
+        String s = "tracecars";
+        System.out.println("act=" + longestPalindrome(s));
+        org.junit.Assert.assertTrue("racecar".equals(longestPalindrome(s)));
+    }
+
+    @Test
+    public void test4() {
+        String s = "aabc";
+        System.out.println("act=" + longestPalindrome(s));
+        org.junit.Assert.assertTrue("aa".equals(longestPalindrome(s)));
+    }
+
+    @Test
+    public void test5() {
         String s = "abcdzdcab";
         String exp = "cdzdc";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test3() {
+    public void test6() {
+        String s = "aa";
+        String exp = "aa";
+        org.junit.Assert.assertEquals(exp, longestPalindrome(s));
+    }
+
+    @Test
+    public void test7() {
+        String s = "abcdzdcab";
+        String exp = "cdzdc";
+        org.junit.Assert.assertEquals(exp, longestPalindrome(s));
+    }
+
+    @Test
+    public void test8() {
         String s = "aba";
         String exp = "aba";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test4() {
+    public void test9() {
         String s = "ab";
         String exp = "a";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test5() {
+    public void test10() {
         String s = "abbbba";
         String exp = "abbbba";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test6() {
+    public void test11() {
         String s = "abbbb";
         String exp = "bbbb";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test7() {
+    public void test12() {
         String s = "abbbbc";
         String exp = "bbbb";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test8() {
+    public void test13() {
         String s = "ccc";
         String exp = "ccc";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 
     @Test
-    public void test9() {
+    public void test14() {
         String s = "abbcde";
         String exp = "bb";
+        org.junit.Assert.assertEquals(exp, longestPalindrome(s));
+    }
+
+    @Test
+    public void test15() {
+        String s = "aaa";
+        String exp = "aaa";
         org.junit.Assert.assertEquals(exp, longestPalindrome(s));
     }
 }
