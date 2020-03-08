8941ca4a659f514823fe8f9bf8f50e686987081b
Steven Tu <sttu@blackberry.com>
2020-03-07 20:49:08 -0500

    修改解答: src/lintcode/_0433_NumberOfIslands.java

--- src/lintcode/_0433_NumberOfIslands.java
+++ src/lintcode/_0433_NumberOfIslands.java
@@ -30,9 +30,52 @@ package lintcode;
  * Output: 1
  */
 public class _0433_NumberOfIslands {
+
+    private static final int[][] DIR = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     
     public int numIslands(boolean[][] grid) {
-        // write your code here
-        return 0;
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        int islands = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] isVisited = new boolean[n][m]; // all initialized as false
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!isVisited[i][j]) {
+                    if (!grid[i][j]) {
+                        isVisited[i][j] = true;
+                    } else { // find the rest of the island and mark as visited
+                        islands++;
+                        // 注意这里不能修改isVisited, 因为会导致markVisited时, 提前退出
+                        markVisited(grid, isVisited, i, j);
+                    }
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private void markVisited(boolean[][]grid, boolean[][] isVisited, int i, int j) {
+        if (!isValid(grid, i, j)  || isVisited[i][j])
+            return;
+
+        isVisited[i][j] = true;
+
+        if (!grid[i][j]) {
+            return;
+        }
+
+        // current on island, continue explain and mark the rest of the island as visited
+        for (int[] dir : DIR) {
+            markVisited(grid, isVisited, i + dir[0], j + dir[1]);
+        }
+    }
+
+    private boolean isValid(boolean[][] grid, int i, int j) {
+        return (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length);
     }
 }
