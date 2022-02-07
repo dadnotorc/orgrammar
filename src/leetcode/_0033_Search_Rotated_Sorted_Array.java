cf1ddf5dbb70585f888937632cd9c2d0453e9771
Steven Tu <dadnotorc@gmail.com>
2022-02-07 17:35:54 -0500

    updated comments

--- src/leetcode/_0033_Search_Rotated_Sorted_Array.java
+++ src/leetcode/_0033_Search_Rotated_Sorted_Array.java
@@ -37,7 +37,64 @@ package leetcode;
  */
 public class _0033_Search_Rotated_Sorted_Array {
 
+    /*
+    暴力解法 - 遍历 O(n)
+
+    二分法 - 针对 nums[mid] < target 或者 > target, 每个又都有两种可能
+    - nums[mid] < target
+       - target 在左上
+          - mid 也在左上 -> l = mid + 1;   <- 这两者一样
+          - mid 在右下 -> r = mid - 1;
+       - target 在右下
+          - mid 也在右下 -> l = mid + 1;   <- 这两者一样
+
+     - nums[mid] > target
+       - target 在左上
+          - mid 也在左上 -> r = mid - 1;   <- 这两者一样
+       - target 在右下
+          - mid 在左上 -> l = mid + 1;
+          - mid 也在右下 -> r = mid - 1;   <- 这两者一样
+
+     所以写 if 的时候, 将单独的一种列出来, 另两种合在一起
+     */
+
     public int search(int[] nums, int target) {
+        if (nums == null || nums.length < 1) {
+            return -1;
+        }
+
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                if (target > nums[r] && nums[mid] < nums[l]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (target < nums[l] && nums[mid] > nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return nums[l] == target ? l : -1;
+    }
+
+
+    /**
+     * l + 1 < r
+     */
+    public int search_1(int[] nums, int target) {
         if (nums == null || nums.length == 0)
             return -1;
 
@@ -48,13 +105,13 @@ public class _0033_Search_Rotated_Sorted_Array {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                if (nums[l] <= target && nums[l] > nums[mid]) { // target在左上, nums[mid]在右下
+                if (target >= nums[l] && nums[mid] < nums[l]) { // target在左上, nums[mid]在右下. 注意 = 的写法
                     r = mid - 1;
                 } else { // nums[mid]和target都在左上, 或者都在右下
                     l = mid + 1;
                 }
             } else { // nums[mid] > target
-                if (nums[l] <= nums[mid] && nums[l] > target) { // nums[mid]在左上, target在右下
+                if (nums[mid] >= nums[l] && target < nums[l]) { // nums[mid]在左上, target在右下
                     l = mid + 1;
                 } else { // target和nums[mid]都在左上, 或者都在右下
                     r = mid - 1;
@@ -69,7 +126,10 @@ public class _0033_Search_Rotated_Sorted_Array {
         return -1;
     }
 
-    // 第二种写法, l <= r
+
+    /**
+     * l <= r
+     */
     public int search_2(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
